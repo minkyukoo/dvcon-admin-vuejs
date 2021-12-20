981c4e5f20ee497d68da469fcf57316fa535c833
SammayadeepPal <sammayadeep@divii.com>
2021-12-20 20:21:47 +0530

    updating

--- vue-admin/src/pages/BannerManagement.vue
+++ vue-admin/src/pages/BannerManagement.vue
@@ -43,35 +43,112 @@
         <div class="card">
             <div class="p-grid">
                 <div class="p-col-12">
-                    <div class="card">
-                        <Toast />
-                        <DataTable :value="customer3" rowGroupMode="subheader" groupRowsBy="representative.name" sortMode="single" sortField="representative.name" :sortOrder="1" scrollable scrollHeight="400px">
-                            <Column field="representative.name" header="Representative"></Column>
-                            <Column field="name" header="Name" style="min-width: 200px"></Column>
-                            <Column field="country" header="Country" style="min-width: 200px">
-                                <template #body="slotProps">
-                                    <img src="assets/demo/flags/flag_placeholder.png" :class="'flag flag-' + slotProps.data.country.code" width="30" />
-                                    <span class="image-text p-ml-2">{{ slotProps.data.country.name }}</span>
-                                </template>
-                            </Column>
-                            <Column field="company" header="Company" style="min-width: 200px"></Column>
-                            <Column field="status" header="Status" style="min-width: 200px">
-                                <template #body="slotProps">
-                                    <span :class="'customer-badge status-' + slotProps.data.status">
-                                        {{ slotProps.data.status }}
-                                    </span>
-                                </template>
-                            </Column>
-                            <Column field="date" header="Date" style="min-width: 200px"></Column>
-                            <template #groupheader="slotProps">
-                                <img :alt="slotProps.data.representative.name" :src="'assets/demo/images/avatar/' + slotProps.data.representative.image" width="32" style="vertical-align: middle" />
-                                <span class="image-text">{{ slotProps.data.representative.name }}</span>
+                    <Toast />
+                    <DataTable :value="products" dataKey="id" responsiveLayout="scroll" scrollable scrollHeight="400px" class="p-mt-3">
+                        <!-- <template #header>
+                                <div class="table-header-container">
+                                    <Button icon="pi pi-plus" label="Expand All" @click="expandAll" class="p-mr-2 p-mb-2" />
+                                    <Button icon="pi pi-minus" label="Collapse All" @click="collapseAll" class="p-mb-2" />
+                                </div>
+                            </template> -->
+                        <!-- <Column :expander="true" headerStyle="width: 3rem" /> -->
+                        <Column field="name" header="Number">
+                            <template #body="slotProps">
+                                <span class="p-column-title">Number</span>
+                                {{ slotProps.data.number }}
                             </template>
-                            <template #groupfooter="slotProps">
-                                <td style="text-align: right" class="p-text-bold p-pr-6">Total Customers: {{ calculateCustomerTotal(slotProps.data.representative.name) }}</td>
+                        </Column>
+                        <Column field="name" header="Title" :sortable="true">
+                            <template #body="slotProps">
+                                <span class="p-column-title">Title</span>
+                                {{ slotProps.data.name }}
                             </template>
-                        </DataTable>
-                    </div>
+                        </Column>
+                        <Column field="name" header="Subtitle" :sortable="true">
+                            <template #body="slotProps">
+                                <span class="p-column-title">Subtitle</span>
+                                {{ slotProps.data.name }}
+                            </template>
+                        </Column>
+                        <Column header="Image">
+                            <template #body="slotProps">
+                                <span class="p-column-title">Image</span>
+                                <img :src="'assets/demo/images/product/' + slotProps.data.image" :alt="slotProps.data.image" class="product-image" />
+                            </template>
+                        </Column>
+                        <Column field="price" header="Banner Position" :sortable="true">
+                            <template #body="slotProps">
+                                <span class="p-column-title">Banner Position</span>
+                                {{ formatCurrency(slotProps.data.bannerPosition) }}
+                            </template>
+                        </Column>
+                        <Column field="category" header="Creation Date" :sortable="true">
+                            <template #body="slotProps">
+                                <span class="p-column-title">Creation Date</span>
+                                {{ formatCurrency(slotProps.data.creationDate) }}
+                            </template></Column
+                        >
+                        <!-- <Column field="rating" header="Reviews" :sortable="true">
+                            <template #body="slotProps">
+                                <span class="p-column-title">Reviews</span>
+                                <Rating :modelValue="slotProps.data.rating" :readonly="true" :cancel="false" />
+                            </template>
+                        </Column>
+                        <Column field="inventoryStatus" header="Status" :sortable="true">
+                            <template #body="slotProps">
+                                <span class="p-column-title">Status</span>
+                                <span :class="'product-badge status-' + (slotProps.data.inventoryStatus ? slotProps.data.inventoryStatus.toLowerCase() : '')">{{ slotProps.data.inventoryStatus }}</span>
+                            </template>
+                        </Column> -->
+                        <Column field="inventoryStatus" header="Status" >
+                            <template #body>
+                                <Button label="correction" icon="pi pi-pencil" iconPos="left" class="p-button p-button-info p-button-sm p-mr-2 p-mb-2"></Button>
+                                <Button label="Delete" icon="pi pi-calendar" iconPos="left" class="p-button p-button-danger p-button-sm p-mr-2 p-mb-2"></Button>
+                            </template>
+                        </Column>
+                        <!-- <template #expansion="slotProps">
+                                <div class="orders-subtable">
+                                    <h5>Orders for {{ slotProps.data.name }}</h5>
+                                    <DataTable :value="slotProps.data.orders" responsiveLayout="scroll">
+                                        <Column field="id" header="Id" :sortable="true">
+                                            <template #body="slotProps">
+                                                <span class="p-column-title">Id</span>
+                                                {{ slotProps.data.id }}
+                                            </template>
+                                        </Column>
+                                        <Column field="customer" header="Customer" :sortable="true">
+                                            <template #body="slotProps">
+                                                <span class="p-column-title">Customer</span>
+                                                {{ slotProps.data.customer }}
+                                            </template>
+                                        </Column>
+                                        <Column field="date" header="Date" :sortable="true">
+                                            <template #body="slotProps">
+                                                <span class="p-column-title">Date</span>
+                                                {{ slotProps.data.date }}
+                                            </template>
+                                        </Column>
+                                        <Column field="amount" header="Amount" :sortable="true">
+                                            <template #body="slotProps" :sortable="true">
+                                                <span class="p-column-title">Amount</span>
+                                                {{ formatCurrency(slotProps.data.amount) }}
+                                            </template>
+                                        </Column>
+                                        <Column field="status" header="Status" :sortable="true">
+                                            <template #body="slotProps">
+                                                <span class="p-column-title">Status</span>
+                                                <span :class="'order-badge order-' + (slotProps.data.status ? slotProps.data.status.toLowerCase() : '')">{{ slotProps.data.status }}</span>
+                                            </template>
+                                        </Column>
+                                        <Column headerStyle="width:4rem">
+                                            <template #body>
+                                                <Button icon="pi pi-search" />
+                                            </template>
+                                        </Column>
+                                    </DataTable>
+                                </div>
+                            </template> -->
+                    </DataTable>
                 </div>
             </div>
         </div>
@@ -79,7 +156,7 @@
 </template>
 
 <script>
-// import ProductService from '../service/ProductService';
+import ProductService from '../service/ProductService';
 import CustomerService from '../service/CustomerService';
 // import {FilterMatchMode,FilterOperator} from 'primevue/api';
 export default {
@@ -96,29 +173,60 @@ export default {
             calendarValue1: null,
             calendarValue2: null,
             customer3: null,
+            customer2: null,
+            products: null,
         };
     },
     customerService: null,
+    productService: null,
     created() {
         this.customerService = new CustomerService();
+        this.productService = new ProductService();
     },
     mounted() {
         this.customerService.getCustomersMedium().then((data) => (this.customer3 = data));
+        this.productService.getProductsWithOrdersSmall().then((data) => (this.products = data));
     },
     methods: {
+        // initFilters1() {
+        //     this.filters1 = {
+        //         global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        //         name: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
+        //         'country.name': { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
+        //         representative: { value: null, matchMode: FilterMatchMode.IN },
+        //         date: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }] },
+        //         balance: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+        //         status: { operator: FilterOperator.OR, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+        //         activity: { value: null, matchMode: FilterMatchMode.BETWEEN },
+        //         verified: { value: null, matchMode: FilterMatchMode.EQUALS },
+        //     };
+        // },
+        // clearFilter1() {
+        //     this.initFilters1();
+        // },
         onRowExpand(event) {
             this.$toast.add({ severity: 'info', summary: 'Product Expanded', detail: event.data.name, life: 3000 });
         },
         onRowCollapse(event) {
             this.$toast.add({ severity: 'success', summary: 'Product Collapsed', detail: event.data.name, life: 3000 });
         },
-        expandAll() {
-            this.expandedRows = this.products.filter((p) => p.id);
-            this.$toast.add({ severity: 'success', summary: 'All Rows Expanded', life: 3000 });
+        // expandAll() {
+        //     this.expandedRows = this.products.filter((p) => p.id);
+        //     this.$toast.add({ severity: 'success', summary: 'All Rows Expanded', life: 3000 });
+        // },
+        // collapseAll() {
+        //     this.expandedRows = null;
+        //     this.$toast.add({ severity: 'success', summary: 'All Rows Collapsed', life: 3000 });
+        // },
+        formatCurrency(value) {
+            return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
         },
-        collapseAll() {
-            this.expandedRows = null;
-            this.$toast.add({ severity: 'success', summary: 'All Rows Collapsed', life: 3000 });
+        formatDate(value) {
+            return value.toLocaleDateString('en-US', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+            });
         },
         calculateCustomerTotal(name) {
             let total = 0;
@@ -136,4 +244,163 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped lang="scss">
+::v-deep(.p-datatable-frozen-tbody) {
+    font-weight: bold;
+}
+
+::v-deep(.p-datatable-scrollable .p-frozen-column) {
+    font-weight: bold;
+}
+
+::v-deep(.p-progressbar) {
+    height: 0.5rem;
+    background-color: #d8dadc;
+
+    .p-progressbar-value {
+        background-color: #607d8b;
+    }
+}
+
+::v-deep(.p-datatable .p-sortable-column .p-column-title) {
+    display: block;
+}
+
+.p-datatable .p-column-filter {
+    display: none;
+}
+
+.table-header {
+    display: flex;
+    justify-content: space-between;
+}
+
+.customer-badge {
+    border-radius: 2px;
+    padding: 0.25em 0.5rem;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 12px;
+    letter-spacing: 0.3px;
+
+    &.status-qualified {
+        background: #c8e6c9;
+        color: #256029;
+    }
+
+    &.status-unqualified {
+        background: #ffcdd2;
+        color: #c63737;
+    }
+
+    &.status-negotiation {
+        background: #feedaf;
+        color: #8a5340;
+    }
+
+    &.status-new {
+        background: #b3e5fc;
+        color: #23547b;
+    }
+
+    &.status-renewal {
+        background: #eccfff;
+        color: #694382;
+    }
+
+    &.status-proposal {
+        background: #ffd8b2;
+        color: #805b36;
+    }
+}
+
+.p-progressbar-value.ui-widget-header {
+    background: #607d8b;
+}
+
+@media (max-width: 640px) {
+    .p-progressbar {
+        margin-top: 0.5rem;
+    }
+}
+
+.product-image {
+    width: 100px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+}
+
+.orders-subtable {
+    padding: 1rem;
+}
+
+.product-badge {
+    border-radius: 2px;
+    padding: 0.25em 0.5rem;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 12px;
+    letter-spacing: 0.3px;
+
+    &.status-instock {
+        background: #c8e6c9;
+        color: #256029;
+    }
+
+    &.status-outofstock {
+        background: #ffcdd2;
+        color: #c63737;
+    }
+
+    &.status-lowstock {
+        background: #feedaf;
+        color: #8a5340;
+    }
+}
+
+.order-badge {
+    border-radius: 2px;
+    padding: 0.25em 0.5rem;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 12px;
+    letter-spacing: 0.3px;
+
+    &.order-delivered {
+        background: #c8e6c9;
+        color: #256029;
+    }
+
+    &.order-cancelled {
+        background: #ffcdd2;
+        color: #c63737;
+    }
+
+    &.order-pending {
+        background: #feedaf;
+        color: #8a5340;
+    }
+
+    &.order-returned {
+        background: #eccfff;
+        color: #694382;
+    }
+}
+.p-datatable {
+    .p-sortable-column {
+        &.p-highlight {
+            &:hover {
+                background: rgb(216 217 243);
+                color: #464df2;
+            }
+        }
+    }
+}
+
+.true-icon {
+    color: #256029;
+}
+
+.false-icon {
+    color: #c63737;
+}
+</style>
