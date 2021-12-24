aaa963675929129d99f143cba2a412ed3f2e8ae3
Madhurimadutta <madhurima@divii.com>
2021-12-24 15:20:19 +0530

    update

--- vue-admin/src/pages/Cmsedit.vue
+++ vue-admin/src/pages/Cmsedit.vue
@@ -11,6 +11,13 @@
                 <div class="p-field p-col-12">
                     <Textarea class="descrition_edit" id="address" rows="4"/>
                 </div>
+                 <div class="button_wrap">
+                    <div class="p-grid p-formgrid">
+                        <div class="p-col-12 p-mb-12 p-lg-12 p-mb-lg-12">
+                            <Button label="Submit" class="p-mr-2 p-mb-2"></Button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -23,8 +30,15 @@ export default {
 <style scoped>
     .descrition_edit{
         width: 100%;
+        height: 150px;
+        resize: none;
     }
     .p-field{
         padding:0 1rem 1rem 0;
     }
+    .button_wrap{
+    width: 100%;
+    text-align: right;
+    margin-top: 30px;
+}
 </style>
