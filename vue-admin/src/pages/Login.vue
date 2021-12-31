e899b6d0c5a90eb1491b982b872772f861c95c15
Bikramjitdivii <bikramjit@divii.com>
2021-12-31 17:39:29 +0530

    add validation

--- vue-admin/src/pages/Login.vue
+++ vue-admin/src/pages/Login.vue
@@ -5,8 +5,15 @@
                 <a @click="goDashboard" class="logo">
                     <img src="assets/layout/images/logo-freya-single.svg" alt="freya-layout" />
                 </a>
-                <InputText id="email" placeholder="Email" v-model="username" />
-                <Password id="password" placeholder="Password" :feedback="false" v-model="password" />
+                <InputText id="email" placeholder="Email" v-model="email" :class="`input-forms ${error.email ? 'p-invalid' : ''}`" />
+                <div class="text-red">{{ error.email }}</div>
+                <Password
+                    id="password"
+                    placeholder="Password"
+                    :feedback="false"
+                    v-model="password"
+                />
+                <div class="text-red">{{ error.password }}</div>
                 <Button label="LOGIN" type="button" @click="Login"></Button>
                 <a href="#">forget password?</a>
                 <!-- <p>Don’t you have an account, <a href="#">sign up</a></p> -->
@@ -20,43 +27,55 @@
 </template>
 
 <script>
+import validateLogin from "../validations/validateLogin";
 import axios from 'axios';
 export default {
     data() {
         return {
-            username: '',
+            email: '',
             password: '',
+            error: {},
         };
     },
     methods: {
         async Login() {
             // localStorage.setItem('token',{username: this.username,password:this.password});
             // window.location = "/"
-            await axios({
-                method: 'post',
-                url: 'http://dvcon-admin-nodejs.dvconsulting.org:4545/dvcon-dev/api/v1/admin/auth/login',
-                data: {
-                    email: this.username,
-                    password: this.password,
-                },
-                headers: {
-                    source: 'dvcon',
-                    apiKey: 'coN21di1202VII01Ed0OnNiMDa2P3p0M',
-                },
-            })
-                .then(function(response) {
-                    console.log(response);
-                    if (response.data.status === 200) {
-                        console.log(response.data.status);
-                        // localStorage.removeItem("expirytoken");
-                        localStorage.setItem('token', response.data.data.token);
-                        window.location = '/';
-                    }
+            let credentials = {
+                email: this.email,
+                password: this.password,
+            };
+            const { isInvalid, error } = validateLogin(credentials);
+            if (isInvalid) {
+                this.error = error;
+                console.log(error);
+            } else {
+                this.error = {};
+                console.log('pass');
+                await axios({
+                    method: 'post',
+                    url: 'http://dvcon-admin-nodejs.dvconsulting.org:4545/dvcon-dev/api/v1/admin/auth/login',
+                    data: credentials,
+                    headers: {
+                        source: 'dvcon',
+                        apiKey: 'coN21di1202VII01Ed0OnNiMDa2P3p0M',
+                    },
                 })
-                .catch(function(response) {
-                    console.log(response);
-                    alert('Email or, password is incorrect');
-                });
+                    .then(function (response) {
+                        console.log(response);
+                        if (response.data.status === 200) {
+                            console.log(response.data.status);
+                            // localStorage.removeItem("expirytoken");
+                            localStorage.setItem('token', response.data.data.token);
+                            window.location = '/';
+                            // this.$router.push({ name: 'dashboard' });
+                        }
+                    })
+                    .catch(function (response) {
+                        console.log(response);
+                        alert('Email or, password is incorrect');
+                    });
+            }
         },
         goDashboard() {
             window.location = '/';
